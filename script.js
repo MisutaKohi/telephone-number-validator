1c2a2ac03d694134d0b1e325fdf0297d895ac810
Ryan Morgan <RyanOnGithub@gmail.com>
2024-02-02 08:10:01 -0800

    Implement validateInput() function

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 const checkButton = document.getElementById('check-btn');
 const clearButton = document.getElementById('clear-btn');
-const output = document.getElementById('output-display');
-const input = document.getElementById('text-input');
+const output = document.getElementById('results-div');
+const input = document.getElementById('user-input');
 
-let pastQueries = []
+const countryCodeRegex = /\d{1} \d{3} \d{3} \d{4}/;
+const dashesRegex = /\d{1} \d{3}-\d{3}-\d{4}/;
+const parenthesesRegex = /\d{1} (\d{3}) \d{3}-\d{4}/;
+const basicNumberRegex = /\d{9}/;
+const basicDashesRegex = /\d{3}-\d{3}-\d{4}/;
+const basicParenthesesRegex = /(\d{3}) \d{3}-\d{4}/;
+const noSpacesRegex = /\d{1}(\d{3})\d{3}-\d{3}/;
+
+const validQueries = [countryCodeRegex, dashesRegex, parenthesesRegex, basicNumberRegex, 
+    basicDashesRegex, basicParenthesesRegex, noSpacesRegex]
+
 
 function validateInput() {
     if (input.value === '') {
         alert('Please provide a phone number');
     }
+    
+    const isValid = validQueries.some((regex) => regex.test(input.value));
+    const validText = isValid ? 'Valid US number:' : 'Invalid US number:';
+
+    let outputHTML = `${validText} ${input.value}`
+    input.value = '';
+    output.innerHTML += outputHTML + '\n';
 
 }
 
 function clearQueries() {
-    pastQueries = [];
     output.innerHTML = ``;
 }
 
